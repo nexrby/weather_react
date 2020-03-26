9a1855cdc9ec5940a5a4b8e08d7a1aa7e587f377
IhorShershkin <shershkin.work@gmail.com>
2020-03-26 10:45:42 +0200

    Updates

---
+++ precache-manifest.25f317eef1cf01b6f95fcc09bb4afcbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8bf1312f2faa0751aee84535ff3d49",
+    "url": "/weather_react/index.html"
+  },
+  {
+    "revision": "230428cb362fad8eabe5",
+    "url": "/weather_react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3a0854603e69662f7a13",
+    "url": "/weather_react/static/css/main.a44c5070.chunk.css"
+  },
+  {
+    "revision": "230428cb362fad8eabe5",
+    "url": "/weather_react/static/js/2.8eaa2290.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather_react/static/js/2.8eaa2290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0854603e69662f7a13",
+    "url": "/weather_react/static/js/main.f59aed5b.chunk.js"
+  },
+  {
+    "revision": "92b6e35a4f6622beff4c",
+    "url": "/weather_react/static/js/runtime-main.886a083c.js"
+  },
+  {
+    "revision": "55f0e397ff8c09dd47aff9a96fc8b3ed",
+    "url": "/weather_react/static/media/info-bg.55f0e397.jpg"
+  }
+]);
